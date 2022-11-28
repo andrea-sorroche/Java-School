718b6c6aaba917d4bbde3813a3a3fea682ffa19c
andrea-sorroche <112932259+andrea-sorroche@users.noreply.github.com>
2022-11-28 15:44:43 +0100

    Create .sql file for database population

--- DatabasePopulation.sql
+++ DatabasePopulation.sql
@@ -1,33 +1,33 @@
 USE railway;
-INSERT INTO user VALUES (1,'andrea', 'password');
+INSERT INTO user(id,login,password) VALUES (1,'andrea', 'password');
 
-INSERT INTO role VALUES (1,'user');
+INSERT INTO role(id,name) VALUES (1,'user');
 
-INSERT INTO user_role VALUES (1,1,1);
+INSERT INTO user_role(id, role_id, user_id) VALUES (1,1,1);
 
-INSERT INTO route VALUES (1);
-INSERT INTO route VALUES (2);
+INSERT INTO route(id) VALUES (1);
+INSERT INTO route(id) VALUES (2);
 
-INSERT INTO train VALUES (1,0,100,'09:00',1,1);
-INSERT INTO train VALUES (2,0,150,'12:00',2,1);
-INSERT INTO train VALUES (3,0,70,'16:00',3,2);
+INSERT INTO train(id,cancelled,count_of_seats, departure_time, number, route_id) VALUES (1,0,100,'09:00',1,1);
+INSERT INTO train(id,cancelled,count_of_seats, departure_time, number, route_id) VALUES (2,0,150,'12:00',2,1);
+INSERT INTO train(id,cancelled,count_of_seats, departure_time, number, route_id) VALUES (3,0,70,'16:00',3,2);
 
-INSERT INTO passenger VALUES (1,'1997-10-23', 'andrea@tsystems.com','Andrea','Sorroche' , '666666666');
+INSERT INTO passenger(id,birth_date,email,first_name,last_name,mobile_phone) VALUES (1,'1997-10-23', 'andrea@tsystems.com','Andrea','Sorroche' , '666666666');
 
-INSERT INTO station VALUES (1,0,'Barcelona', '00:30');
-INSERT INTO station VALUES (2,0,'Madrid', '00:30');
-INSERT INTO station VALUES (3,0,'Malaga', '00:30');
-INSERT INTO station VALUES (4,0,'Sevilla', '00:20');
-INSERT INTO station VALUES (5,0,'Cordoba', '00:15');
+INSERT INTO station(id,closed,name,stop_duration) VALUES (1,0,'Barcelona', '00:30');
+INSERT INTO station(id,closed,name,stop_duration) VALUES (2,0,'Madrid', '00:30');
+INSERT INTO station(id,closed,name,stop_duration) VALUES (3,0,'Malaga', '00:30');
+INSERT INTO station(id,closed,name,stop_duration) VALUES (4,0,'Sevilla', '00:20');
+INSERT INTO station(id,closed,name,stop_duration) VALUES (5,0,'Cordoba', '00:15');
 
-INSERT INTO route_part VALUES (1,10,"09:00",1, 2);
-INSERT INTO route_part VALUES (2,30,"11:00",2, 3);
-INSERT INTO route_part VALUES (3,10,"12:00",1, 2);
-INSERT INTO route_part VALUES (4,30,"14:00",2, 3);
-INSERT INTO route_part VALUES (5,20,"16:00",4, 5);
+INSERT INTO route_part(id,price,time_passing,station_from,station_to) VALUES (1,10,"09:00",1, 2);
+INSERT INTO route_part(id,price,time_passing,station_from,station_to) VALUES (2,30,"11:00",2, 3);
+INSERT INTO route_part(id,price,time_passing,station_from,station_to) VALUES (3,10,"12:00",1, 2);
+INSERT INTO route_part(id,price,time_passing,station_from,station_to) VALUES (4,30,"14:00",2, 3);
+INSERT INTO route_part(id,price,time_passing,station_from,station_to) VALUES (5,20,"16:00",4, 5);
 
-INSERT INTO route_connection VALUES (1,1,1);
-INSERT INTO route_connection VALUES (2,1,2);
+INSERT INTO route_connection(id, route_id, route_part_id) VALUES (1,1,1);
+INSERT INTO route_connection(id, route_id, route_part_id) VALUES (2,1,2);
 
 
 
