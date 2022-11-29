04f4f84e128c5517404a2f29114543ab1ed2599d
andrea-sorroche <112932259+andrea-sorroche@users.noreply.github.com>
2022-11-29 13:06:33 +0100

    Passenger controller and service

--- railway/src/main/java/com/javaschool/railway/controller/PassengerController.java
+++ railway/src/main/java/com/javaschool/railway/controller/PassengerController.java
@@ -1,4 +1,58 @@
 package com.javaschool.railway.controller;
 
+import com.javaschool.railway.entity.Passenger;
+import com.javaschool.railway.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/passenger")
 public class PassengerController {
+    @Autowired
+    private PassengerService service;
+
+    //EMPTY CALL
+    @GetMapping("")
+    String simpleResponse() {
+        return("Empty call");
+    }
+
+    //GET ALL
+    @GetMapping("/all")
+    public ResponseEntity<List<Passenger>> getAllPassengers(){
+        List<Passenger> passengers = service.findAll();
+        return new ResponseEntity<>(passengers, HttpStatus.OK);
+    }
+
+    //GET BY ID
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Passenger> getById(@PathVariable("id") Integer id){
+        Passenger passenger = service.findById(id);
+        return new ResponseEntity<>(passenger, HttpStatus.OK);
+    }
+
+    //POST NEW ITEM
+    @PostMapping("/add")
+    public  ResponseEntity<Passenger> addPassenger(@RequestBody Passenger passenger){
+        Passenger newPassenger = service.create(passenger);
+        return new ResponseEntity<>(newPassenger, HttpStatus.CREATED);
+    }
+
+    //UPDATE BY ID
+    @PutMapping("/update")
+    public  ResponseEntity<Passenger> updateById(@RequestBody Passenger passenger){
+        Passenger updatePassenger = service.updateById(passenger.getId(), passenger);
+        return new ResponseEntity<>(updatePassenger, HttpStatus.OK);
+    }
+
+    //DELETE BY ID
+    @DeleteMapping("/delete/{id}")
+    public  ResponseEntity<?> deleteById(@PathVariable("id") Integer id){
+        service.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- railway/src/main/java/com/javaschool/railway/service/PassengerService.java
+++ railway/src/main/java/com/javaschool/railway/service/PassengerService.java
@@ -1,4 +1,86 @@
 package com.javaschool.railway.service;
 
+import com.javaschool.railway.entity.Passenger;
+import com.javaschool.railway.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
 public class PassengerService {
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    //GET BY ID
+    public Passenger findById(Integer id) {
+        Optional<Passenger> result = null;
+        result = passengerRepository.findById(id);
+        if(result.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return (result.get());
+    }
+
+    //GET ALL ITEMS
+    public List<Passenger> findAll() throws NoSuchElementException {
+        List<Passenger> result = null;
+        result = passengerRepository.findAll();
+        if (result.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return (result);
+        }
+    }
+
+    //POST NEW ITEM
+    public Passenger create(Passenger passenger) throws IllegalArgumentException{
+        Passenger result = null;
+        if(passenger.getFirstName() != null && passenger.getLastName() != null && passenger.getEmail() != null &&
+                passenger.getBirthDate() != null && passenger.getMobilePhone() != null) {
+            result = passengerRepository.save(passenger);
+            return(result);
+        }else {
+                throw new IllegalArgumentException();
+            }
+    }
+
+    //UPDATE BY ID
+    public Passenger updateById(Integer id, Passenger passenger) throws NoSuchElementException, IllegalArgumentException {
+        Optional<Passenger> test = passengerRepository.findById(id);
+        if (test.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        if (passenger.getFirstName() != null)
+            test.get().setFirstName(passenger.getFirstName());
+        if (passenger.getLastName() != null)
+            test.get().setLastName(passenger.getLastName());
+        if (passenger.getBirthDate() != null)
+            test.get().setBirthDate(passenger.getBirthDate());
+        if (passenger.getEmail() != null)
+            test.get().setEmail(passenger.getEmail());
+        if (passenger.getMobilePhone() != null)
+            test.get().setMobilePhone(passenger.getMobilePhone());
+
+        Passenger result = passengerRepository.save(test.get());
+        return (result);
+    }
+
+    //DELETE BY ID
+    public Passenger deleteById(Integer id) throws NoSuchElementException{
+        Optional<Passenger> test = passengerRepository.findById(id);
+        if(test.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        passengerRepository.deleteById(id);
+        return (test.get());
+    }
+
+
+
 }
+
