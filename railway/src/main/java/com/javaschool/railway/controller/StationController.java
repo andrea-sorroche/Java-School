b643c47174d47bbb171efcba97c38225d20df46e
andrea-sorroche <112932259+andrea-sorroche@users.noreply.github.com>
2022-11-30 09:34:57 +0100

    Station controller and service

--- railway/src/main/java/com/javaschool/railway/controller/StationController.java
+++ railway/src/main/java/com/javaschool/railway/controller/StationController.java
@@ -1,4 +1,58 @@
 package com.javaschool.railway.controller;
 
+import com.javaschool.railway.entity.Station;
+import com.javaschool.railway.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/station")
 public class StationController {
+    @Autowired
+    private StationService service;
+
+    //EMPTY CALL
+    @GetMapping("")
+    String simpleResponse() {
+        return("Empty call");
+    }
+
+    //GET ALL
+    @GetMapping("/all")
+    public ResponseEntity<List<Station>> getAllStations(){
+        List<Station> stations = service.findAll();
+        return new ResponseEntity<>(stations, HttpStatus.OK);
+    }
+
+    //GET BY ID
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Station> getById(@PathVariable("id") Integer id){
+        Station station = service.findById(id);
+        return new ResponseEntity<>(station, HttpStatus.OK);
+    }
+
+    //POST NEW ITEM
+    @PostMapping("/add")
+    public  ResponseEntity<Station> addStation(@RequestBody Station station){
+        Station newStation = service.create(station);
+        return new ResponseEntity<>(newStation, HttpStatus.CREATED);
+    }
+
+    //UPDATE BY ID
+    @PutMapping("/update")
+    public  ResponseEntity<Station> updateById(@RequestBody Station station){
+        Station updateStation = service.updateById(station.getId(), station);
+        return new ResponseEntity<>(updateStation, HttpStatus.OK);
+    }
+
+    //DELETE BY ID
+    @DeleteMapping("/delete/{id}")
+    public  ResponseEntity<?> deleteById(@PathVariable("id") Integer id){
+        service.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- railway/src/main/java/com/javaschool/railway/service/StationService.java
+++ railway/src/main/java/com/javaschool/railway/service/StationService.java
@@ -1,4 +1,78 @@
 package com.javaschool.railway.service;
 
+import com.javaschool.railway.entity.Passenger;
+import com.javaschool.railway.entity.Station;
+import com.javaschool.railway.repository.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
 public class StationService {
+
+    @Autowired
+    StationRepository stationRepository;
+
+    //GET BY ID
+    public Station findById(Integer id) {
+        Optional<Station> result = null;
+        result = stationRepository.findById(id);
+        if(result.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return (result.get());
+    }
+
+    //GET ALL ITEMS
+    public List<Station> findAll() throws NoSuchElementException {
+        List<Station> result = null;
+        result = stationRepository.findAll();
+        if (result.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return (result);
+        }
+    }
+
+    //POST NEW ITEM
+    public Station create(Station station) throws IllegalArgumentException{
+        Station result = null;
+        if(station.getName() != null && station.isClosed() != null && station.getStopDuration() != null) {
+            result = stationRepository.save(station);
+            return(result);
+        }else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    //UPDATE BY ID
+    public Station updateById(Integer id, Station station) throws NoSuchElementException, IllegalArgumentException {
+        Optional<Station> test = stationRepository.findById(id);
+        if (test.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        if (station.getName() != null)
+            test.get().setName(station.getName());
+        if (station.getStopDuration()!= null)
+            test.get().setStopDuration(station.getStopDuration());
+        if (station.isClosed()!= null)
+            test.get().setClosed(station.isClosed());
+
+        Station result = stationRepository.save(test.get());
+        return (result);
+    }
+
+    //DELETE BY ID
+    public Station deleteById(Integer id) throws NoSuchElementException{
+        Optional<Station> test = stationRepository.findById(id);
+        if(test.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        stationRepository.deleteById(id);
+        return (test.get());
+    }
 }
